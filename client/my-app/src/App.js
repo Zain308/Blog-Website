f7af11a73bc90fb321991745dd8504b63bbb402a
zain <mr.zain903@gmail.com>
2025-04-27 15:15:52 +0500

    Added new features / updated files / fixed bugs

---
+++ client/my-app/src/App.js
@@ -0,0 +1,25 @@
+import './App.css';
+import Header from './Header';
+import Post from './Post';
+import {Route, Routes} from "react-router-dom";
+function App() {
+  return (
+    <Routes>
+      <Route index element={
+        <main>
+        <Header />
+
+       <Post />
+       <Post />
+       <Post />
+
+        
+      </main>
+
+      } />
+
+    </Routes>
+          );
+}
+
+export default App;
---
+++ client/my-app/src/Post.js
@@ -0,0 +1,25 @@
+export default function Post(){
+    return(
+        <div className="post">
+        <div className="image">
+          <img src="https://techcrunch.com/wp-content/uploads/2025/04/Inspo_Truck_web.jpg?resize=1200,800" /> 
+          </div>
+          <div className="texts"> 
+          <h2>
+          Bezos-backed Slate Auto debuts analog EV pickup truck that is decidedly anti-Tesla
+          </h2>
+
+          <p className="info">
+             <a className="author">Zain</a>
+             <time>2025-4-26</time>
+          </p>
+
+          <p className="summary">A new American electric vehicle startup called Slate Auto has made its debut, and it’s about as anti-Tesla as it gets.
+
+          It’s affordable, deeply customizable, and very analog. It has manual windows and it doesn’t come with a main infotainment screen. Heck, it isn’t even painted. It can also transform from a two-seater pickup to a five-seater SUV.</p>
+
+          </div>
+          
+      </div>
+    );
+}
